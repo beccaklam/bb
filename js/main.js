4cba63150210f50408cbcabfa4af0d0b44b300fa
beccaklam <rebeccal@mozillafoundation.org>
2017-03-22 00:14:17 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,5 +3,6 @@ $(document).ready(function() {
 				anchors: ['firstPage', 'secondPage', '3rdPage'],
 				sectionsColor: ['#000033', '#33ffcc', '#000033'],
 				slidesNavigation: true,
+				scrollBar: true
 			});
 		});
